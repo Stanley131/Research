0ad66d04d2b63a75de7b668404a98875b35f5a78
Stanley Ye <yechunhou@gmail.com>
2020-03-14 03:11:23 -0400

    remove unncessary files

--- Research_2019/RpcTransportDesign/OMNeT++Simulation/inet/tests/fingerprint/runWirelessTests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./fingerprints omnet4/*.csv -m wireless -m adhoc -m manet -m mobileipv6 -m WirelessDHCP
--- Research_2019/RpcTransportDesign/OMNeT++Simulation/inet/tests/module/runWirelessTests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./runtest ReceptionState_*.test Interference_*.test Ieee80211_*.test
